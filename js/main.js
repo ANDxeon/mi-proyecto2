a1246bbbbccc122c60af11bcb34502d86945838f
ANDxeon <andresxeon@gmail.com>
2023-07-03 18:52:47 -0500

    modo dark

--- js/main.js
+++ js/main.js
@@ -5,10 +5,27 @@ console.log(menuItems);
 //Recorrer la lista de nodos
 menuItems.forEach(function (item) {
     //escuchar el click en todos
-    item.addEventListener("click", funtion (e){
+    item.addEventListener("click", function (e){
     const currentItem = document.querySelector(".active");
     currentItem.classList.remove("active");
     e.target.classList.add("active");
 
     })
 })
+
+//MODO OSCURO
+const themeSwitcher = document.querySelector("#theme-switcher");
+let darkMode = false;
+const iconos = document.querySelectorAll(".redes")
+
+themeSwitcher.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+    darkMode = !darkMode;
+
+    if (darkMode) {
+        themeSwitcher.innerHTML = "Cambiar a tema claro";
+    } else {
+        themeSwitcher.innerHTML = "Cambiar a tema oscuro";
+    }
+
+});
